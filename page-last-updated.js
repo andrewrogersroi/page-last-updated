c827aae151f6a5f9289fe8ecd3d621f9375a29b4
andrewrogersroi <andrew@cleriti.com>
2016-08-25 12:37:11 -0400

    Update page-last-updated.js

--- page-last-updated.js
+++ page-last-updated.js
@@ -1,33 +1,6 @@
-function getPageUpdatedDate()
-{
-    
-    $.ajax({
-        type: "GET",
-        url: "http://www.yoursiteurl.com/sitemap.xml",
-        cache: false,
-        dataType: "xml",
-        success: function(xml) {
-            console.log(xml)
-            var cururl = window.location.href; 
-            var lastChar = cururl.slice(-1);
-            if(lastChar == '/'){
-               cururl = cururl.slice(0, -1);
-            }
-            
-            $(xml).find('url').each(function(){
-                var url = $(this).find("loc").text().trim();
-                var date = $(this).find("lastmod").text();
-                if(cururl == url){
-                    
-                    var d = new Date(date.replace(/-/g, "/"));
-                    var curr_date = d.getDate();
-                    var curr_month = d.getMonth();
-                    curr_month++;
-                    var curr_year = d.getFullYear();
-                    date = curr_month + "/" + curr_date + "/" + curr_year;
-                    $('.last-update span').html('Page Last Updated: '+ date);
-                }
-            });
-        }
-    });
-}
+<script type="text/javascript">
+    var updated = new Date({{content.updated}});
+    var created = new Date({{content.created}});
+    var publish_date = "{{content.publish_date}}";
+    document.getElementById("lastUpdated").innerHTML = ('Page last updated:') + (updated.getMonth()+1)+"/"+updated.getDate()+"/"+updated.getFullYear();
+</script>
